3257fcb411d019dfc16e43599d645056d6acb096
jjosei <oseisarfohjesse@gmail.com>
2023-05-12 22:54:13 +0000

    Added a linked list to be able to store mutiple instances of Aircraft.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         Aircraft helicopter = new Aircraft();
-        FlyingObjects flyerr = helicopter;
         movement muevee = helicopter;
 
 
-        muevee.accelerate();
+        //muevee.accelerate();
+        //muevee.land();
 
+        LinkedList<movement> locomote = new LinkedList<>();
+        locomote.add(helicopter);
+        locomote.add(muevee);
 
-        muevee.land();
+        takeoffer(locomote);
+        accelerator(locomote);
+        lander(locomote);
+
+    }
+
+    private static void takeoffer(LinkedList<movement> muevee) {
+
+        for (var vehicle : muevee) {
+            vehicle.takeoff();
+        }
+    }
+
+    private static void accelerator(LinkedList<movement> muevee) {
+
+        for (var vehicle : muevee) {
+            vehicle.accelerate();
+        }
+    }
+
+    private static void lander(LinkedList<movement> muevee) {
+
+        for (var vehicle : muevee) {
+            vehicle.land();
+        }
     }
 }
